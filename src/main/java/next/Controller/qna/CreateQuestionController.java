52ffc4503cc147e56462210f913c3bddd37d3a8a
zzangoobrother <com1368@naver.com>
2021-07-11 21:26:46 +0900

    qna 로그인 후 등록 가능 구현

--- src/main/java/next/Controller/qna/CreateQuestionController.java
+++ src/main/java/next/Controller/qna/CreateQuestionController.java
@@ -4,6 +4,8 @@ import core.mvc.AbstractController;
 import core.mvc.ModelAndView;
 import next.dao.QuestionDao;
 import next.model.Question;
+import next.model.User;
+import next.web.UserSessionUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +14,11 @@ public class CreateQuestionController extends AbstractController {
     private QuestionDao questionDao = new QuestionDao();
     @Override
     public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        Question question = new Question(request.getParameter("writer"), request.getParameter("title"),
+        if (!UserSessionUtils.isLogin(request.getSession())) {
+            return jspView("redirect:/users/loginForm");
+        }
+        User user = UserSessionUtils.getUserFromSession(request.getSession());
+        Question question = new Question(user.getUserId(), request.getParameter("title"),
                             request.getParameter("contents"));
         questionDao.insert(question);
         return jspView("redirect:/");
